02c71165ddea29235352cfc2494ace0260e1b1dc
Eliézer Fernandes <eliezer.fernandes@zup.com.br>
2020-10-30 14:40:47 -0300

    adicionando metodo findByResultadoPropostaStatus na PropostaRepository -ajustando outros arquivos

--- src/main/java/br/com/zup/bootcamp/proposta/service/feign/AnalisePropostaCliente.java
+++ src/main/java/br/com/zup/bootcamp/proposta/service/feign/AnalisePropostaCliente.java
@@ -1,14 +1,15 @@
-package br.com.zup.bootcamp.proposta.service;
+package br.com.zup.bootcamp.proposta.service.feign;
 
 import br.com.zup.bootcamp.proposta.request.AnalisePropostaRequest;
 import br.com.zup.bootcamp.proposta.response.ResultadoAnaliseResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "analiseProposta", url = "${servicos-externos.base-url}")
 public interface AnalisePropostaCliente {
 
     @PostMapping("/api/solicitacao")
-    ResponseEntity<ResultadoAnaliseResponse> getAnalise(AnalisePropostaRequest request);
+    ResponseEntity<ResultadoAnaliseResponse> getAnalise(@RequestBody AnalisePropostaRequest request);
 }
