b12e2171b4b04a5be79bd982824da5fac2cd5076
Eliézer Fernandes <eliezer.oliveirasiueg@gmail.com>
2020-10-30 09:45:26 -0300

    Refatorando análise da proposta - Tratramento do retorno 422 - Adicionando Service para realizar avaliação da proposta(simplificando a complexidade do PropostaService)

--- src/main/java/br/com/zup/bootcamp/proposta/service/PropostaService.java
+++ src/main/java/br/com/zup/bootcamp/proposta/service/PropostaService.java
@@ -1,8 +1,12 @@
 package br.com.zup.bootcamp.proposta.service;
 
+import br.com.zup.bootcamp.proposta.enums.AnalisePropostaStatus;
 import br.com.zup.bootcamp.proposta.model.Proposta;
 import br.com.zup.bootcamp.proposta.repository.PropostaRepository;
 import br.com.zup.bootcamp.proposta.validators.DocumentoUnicoValidator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,25 +16,30 @@ public class PropostaService {
 
     private final PropostaRepository propostaRepository;
     private final DocumentoUnicoValidator valorUnicoValidator;
-    private final Integracoes solicitacaoAnalise;
+    private final AvaliaProposta avaliaProposta;
 
-    public PropostaService(PropostaRepository propostaRepository, DocumentoUnicoValidator valorUnicoValidator, Integracoes solicitacaoAnalise) {
+    private final Logger log = LoggerFactory.getLogger(PropostaService.class);
+
+    public PropostaService(PropostaRepository propostaRepository, DocumentoUnicoValidator valorUnicoValidator,
+                           AvaliaProposta avaliaProposta) {
         this.propostaRepository = propostaRepository;
         this.valorUnicoValidator = valorUnicoValidator;
-        this.solicitacaoAnalise = solicitacaoAnalise;
+        this.avaliaProposta = avaliaProposta;
     }
 
-    public Proposta criar(Proposta proposta) {
+    public Proposta criar(Proposta proposta) throws JsonProcessingException {
 
         if (!valorUnicoValidator.isValid(proposta)){
             throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
         }
 
         final var propostaSalva = this.propostaRepository.save(proposta);
+        final var analisePropostaRequest = proposta.toAnalisePropostaRequest();
 
-        //final var analisePropostaRequest = proposta.toAnalisePropostaRequest();
+        AnalisePropostaStatus avaliacao = this.avaliaProposta.avaliar(analisePropostaRequest).getResultadoSolicitacao();
 
-        //String s = this.solicitacaoAnalise.getAnalisa(analisePropostaRequest);
+        propostaSalva.atualizarStatus(avaliacao.toPropostaStatus());
+        this.propostaRepository.save(propostaSalva);
 
         return propostaSalva;
     }
